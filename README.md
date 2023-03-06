69a9ea08dd2f2845b4ceaa8d905ff5e22dc7a93d
mac <mac@MacBook-Pro.local>
2023-03-06 14:49:12 +0300

    update

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
-# posz92printer
+git# posz92printer
 
-A new Flutter plugin project.
+flutter project for printing on pos z91 & z92 printer
 
 ## Getting Started
 
-This project is a starting point for a Flutter
-[plug-in package](https://flutter.dev/developing-packages/),
-a specialized package that includes platform-specific implementation code for
-Android and/or iOS.
+you can use this plugin to print on pos z91 & z92 printer
 
-For help getting started with Flutter development, view the
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Usage
+
+import 'package:posz92printer/posz92printer.dart';
+
+// To print text
+Posz92printer.printText("Hello World");
+
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'package:posz92printer/posz92printer.dart';
 
 void main() {
@@ -80,61 +79,12 @@ class _MyAppState extends State<MyApp> {
                   //   fontSize: 45,
                   // );
                   // await _printObj.printLine();
-                  await _printObj.print3Column(
-                    textLeft: "Amount",
-                    textCenter: "5000IQD",
-                    textRight: "بڕی",
-                    spaceLeft: 20,
-                    spaceCenter: 45,
-                    spaceRight: 10,
+                  await _printObj.printText(
+                    text: "HELLO WORLD",
                     isBold: true,
                     fontSize: 20,
                   );
-                  await _printObj.print2Column(
-                    textLeft: "code",
-                    textRight: "کۆد",
-                    spaceLeft: 55,
-                    spaceRight: 30,
-                    isBold: true,
-                    fontSize: 18,
-                  );
-                  // await _printObj.printSpace(space: 1);
-                  // await _printObj.printText(
-                  //   text: "12437476326346",
-                  //   alignment: AlignmentPrint.center,
-                  //   isBold: true,
-                  //   fontSize: 38,
-                  // );
-                  // await _printObj.printSpace(space: 1);
-                  // await _printObj.printText(
-                  //   text:
-                  //       "hi this is a test for print text. \n سڵاو ایا باڕسڵاو ایا باڕ ام سڵاو \n یەکەم دووەم سیەکەم دووەم سێیەم",
-                  //   alignment: AlignmentPrint.center,
-                  //   isBold: true,
-                  //   fontSize: 24,
-                  // );
-                  // await _printObj.printLine();
 
-                  await _printObj.print3Column(
-                    textLeft: "serial",
-                    textCenter: "2134-12-12122",
-                    textRight: "زنجیرە",
-                    spaceLeft: 10,
-                    spaceCenter: 40,
-                    spaceRight: 10,
-                    isBold: false,
-                    fontSize: 20,
-                  );
-                  await _printObj.print3Column(
-                    textLeft: "date",
-                    textCenter: "6757776745",
-                    textRight: "ڕۆژ",
-                    spaceLeft: 15,
-                    spaceCenter: 55,
-                    spaceRight: 15,
-                    isBold: false,
-                    fontSize: 20,
-                  );
                   // await _printObj.print3Column(
                   //   textLeft: "office",
                   //   textCenter: "zamo phone",
@@ -177,13 +127,6 @@ class _MyAppState extends State<MyApp> {
                   // await _printObj.printSpace(space: 2);
 
                   // get image from network and convert to Uint8List
-                  final response = await http.get(Uri.parse(
-                      "https://i.ibb.co/bzjwNBz/2023-03-06-14-12-38-1.jpg"));
-                  final bytes = response.bodyBytes;
-                  await _printObj.printBitMap(
-                    pathImage: bytes,
-                  );
-                  await _printObj.printSpace(space: 2);
                 },
               )
             ],
